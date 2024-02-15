87a51e0e5c377cc00c1a95d0dd0b08e7ccaefefc
Piotr Paczosa <PIPC@simcorp.com>
2024-02-15 15:52:04 +0100

    Added message for console

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello mars");
+            Console.WriteLine("Hellow mars again");
         }
         
     }
